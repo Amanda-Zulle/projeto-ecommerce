db57086c40903ed3ca8420e5a3df85b2bc65fe9e
Amanda zulle <amandazulle86@gmail.com>
2026-04-13 14:13:12 +0100

    aula 10 refatoração separando em serviços

--- projeto-ecommerce-dv/js/menu.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const botaoMenu = document.querySelector(".menu-hamburguer");
-    const cabecalho = document.querySelector(".cabecalho");
-
-    botaoMenu.addEventListener("click", () => {
-        cabecalho.classList.toggle("menu-ativo");
-    });
-});
